bff27659ee9cb092212574e0a77387a89459781b
Navya <navyasri.annamreddy@veegam.com>
2025-10-17 10:29:37 +0530

    GA4 iNTEGRATION

--- android/app/src/main/java/altware/dynamitenews/MainActivity.kt
+++ android/app/src/main/java/altware/dynamitenews/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.altware.dynamitenews;
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : ReactActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // ✅ Initialize Firebase Analytics
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        // ✅ Log event when app opens
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.METHOD, "App Launched")
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, bundle)
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
